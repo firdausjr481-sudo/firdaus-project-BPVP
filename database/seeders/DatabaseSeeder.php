c8ff21b68c3756bea04e9668716c72875a13aeb6
firdausjr481-sudo <firdausjr481@gmail.com>
2026-04-20 15:24:51 +0700

    Day 13 templating

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Firdaus',
+            'email' => 'testing@example.com',
+            'password' => bcrypt('11223344'),
         ]);
     }
 }
